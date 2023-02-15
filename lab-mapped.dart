5383de139d2ef377e0c3ead322ebc50e04d4ff86
kanticha954 <30949247+kanticha954@users.noreply.github.com>
2023-02-15 10:55:21 +0700

    Update lab-mapped.dart

--- lab-mapped.dart
+++ lab-mapped.dart
@@ -3,8 +3,17 @@ void main(){
   Map<String, String> color = {"red":"แดง", "green":"เขียว"};
   print(color);
   
-  color["Blue"] = "น้ำเงิน";
-  print(color);
+  color["Blue"] = "น้ำเงิน";  //add in map
+  print(color); 
+  color["Pink"] = "ชมพู";
+  
+  color.remove("green"); //remove from map
+  print(color); 
+
+  
+  List<String> data = ['A','B','C'];
+  Map<int, String> item = data.asMap(); //List to Map
+  print(item);
     
 
 }
